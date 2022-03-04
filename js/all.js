3c49f2f1b2d63e9f2cfe94ad797177af1c5e8764
SHiuN26 <justodme26@gmail.com>
2022-03-04 21:36:53 +0800

    1

---
+++ js/all.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        speed: 800,
+        autoplay: {
+            delay: 500
+        },
+        
+
+        //If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        
+    });
+    $('.menu > li > a').click(function (e) { 
+        e.preventDefault();
+        $(this).parent().siblings().find('ul').slideUp();
+        $(this).parent().find('ul').slideToggle();
+    });
+});
+// $('.cart > li > a').click(function(event) {
+//     event.preventDefault();
+//     $(this).parent().siblings().find('ul').slideUp();
+//     $(this).parent().find('ul').slideToggle();
+//   });
